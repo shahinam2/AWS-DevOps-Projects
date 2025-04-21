a57f605ac9aeb2eee7f61599e601a1ed41b69f81
Shahin <shahin.ccie1989@gmail.com>
2025-04-21 20:43:58 +0200

    Fix: add debug echo statements for directory names during Lambda packaging and remove unused authentication Lambda function

--- 05_Recipe_Sharing_App_Serverless_Edition/lambdas/testauth/index.py
+++
@@ -1,7 +0,0 @@
-import json
-def lambda_handler(event, context):
-    response = {
-        "statusCode": 200,
-        "body": json.dumps({"message": "You've passed the authentication token"})
-    }
-    return response
