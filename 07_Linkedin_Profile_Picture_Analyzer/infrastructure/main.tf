480467fcee7a01bc9f3d96fc424ff5bd8792c64d
Shahin <shahin.ccie1989@gmail.com>
2025-05-04 12:28:06 +0200

    Fix: add missing quotes around hardcoded S3 bucket name and region in main.tf

--- 07_Linkedin_Profile_Picture_Analyzer/infrastructure/main.tf
+++ 07_Linkedin_Profile_Picture_Analyzer/infrastructure/main.tf
@@ -8,9 +8,9 @@ provider "aws" {
 # You have to hardcode the bucket name and region here
 terraform {
   backend "s3" {
-    bucket       = terraform-state-bucket-593793041840
+    bucket       = "terraform-state-bucket-593793041840"
     key          = "terraform.tfstate"
-    region       = eu-central-1
+    region       = "eu-central-1"
     use_lockfile = false # Enable this when you are in a team
   }
 }
